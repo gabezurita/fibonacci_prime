98aab457ca42a90fc7a0d263578e3ab1874ecf6b
Gabriel Zurita <gabezurita@gmail.com>
2018-05-24 23:43:23 -0400

    add first n fibonacci numbers and make format prettier

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @fibonacci_result = Fibonacci.calculate(params[:n])
-    @prime_numbers_result = PrimeNumbers.output_first(params[:n])
+    @num = params[:n].to_i
+
+    @fibonacci_result = Fibonacci.calculate(@num)
+
+    @first_n_fibonacci_numbers = Cache::FIBONACCI.sort_by { |k,v| k }.first(@num).to_h.values.join(' ')
+
+    @prime_numbers_result = PrimeNumbers.output_first(@num).join(' ')
   end
 end
