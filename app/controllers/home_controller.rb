b30465d8e835aeb2ce70bd03867e92cc397a31ad
Gabriel Zurita <gabezurita@gmail.com>
2018-05-25 09:11:00 -0400

    remove threading for heroku app

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,10 @@ class HomeController < ApplicationController
   def index
     @num = params[:n].to_i
 
-    @prime_numbers_result = PrimeNumbers.output_first(@num).join(' ')
-
-    Thread.new { @fibonacci_result = Fibonacci.calculate(@num) }
+    @fibonacci_result = Fibonacci.calculate(@num)
 
     @first_n_fibonacci_numbers = Cache::FIBONACCI.sort_by { |k,v| k }.first(@num).to_h.values.join(' ')
+
+    @prime_numbers_result = PrimeNumbers.output_first(@num).join(' ')
   end
 end
